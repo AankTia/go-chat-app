432e71e5f65eb42286bc5cd1bfa36dbea3dd7155
TiaWidi <tia.septianawidi@gmail.com>
2025-04-15 00:18:34 +0700

    Modeling a room

--- room.go
+++ room.go
@@ -4,4 +4,10 @@ type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other client
 	forward chan []byte
+	// join a channel for clients wishig to join the room.
+	join chan *client
+	// leave is a channel for clients whising to leave the room.
+	leave chan *client
+	// clients hold all current clients in this room.
+	clients map[*client]bool
 }
