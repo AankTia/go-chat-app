8cde4c8a875fe55a1498559240ce81840c293152
TiaWidi <tia.septianawidi@gmail.com>
2025-04-15 23:30:36 +0700

    Update loginHandler

--- chat/main.go
+++ chat/main.go
@@ -10,6 +10,10 @@ import (
 	"text/template"
 
 	"github.com/AankTia/chat-app/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 // templ represents a single tenplate
@@ -30,6 +34,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application")
 	flag.Parse()
+
+	// setup gomniauth
+	// gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
+	gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("key", "secret","http://localhost:8080/auth/callback/google"),
+	)
 
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
